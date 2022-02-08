953794810863fe0571aafb8799a5c7302b7cc87b
tadrochlinski <tadrochlinski@gmail.com>
2022-02-08 10:27:45 +0100

    js done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-window.onload = () =>{
-    console.log('hello');
-}
+const shareBtn = document.querySelector('#shareBtn');
+const shareSection = document.querySelector('#shareSection');
+
+shareBtn.addEventListener('click', ()=>{
+   shareBtn.classList.toggle('articlePreviev__shareBtn--active'); 
+   shareSection.classList.toggle('articlePreviev__shareSection--active');
+});
